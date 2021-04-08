80ef5cc02f3dfb43c084bf8ed9d9af6de82f22e8
Viktor Modroczky <v.modroczky@gmail.com>
2021-04-08 14:31:47 +0200

    Added AccountActivity

---
+++ app/src/main/java/com/mtaa/techtalk/activities/AccountActivity.kt
@@ -0,0 +1,15 @@
+package com.mtaa.techtalk.activities
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class AccountActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+
+        }
+    }
+}
