38deec48f25fc90c539d8e0ffab74e81d9b3a916
Viktor Modroczky <v.modroczky@gmail.com>
2021-04-08 18:36:53 +0200

    Added trust score to account screen

--- app/src/main/java/com/mtaa/techtalk/activities/AccountActivity.kt
+++ app/src/main/java/com/mtaa/techtalk/activities/AccountActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -13,8 +14,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
@@ -59,6 +62,7 @@ class AccountActivity : ComponentActivity() {
 
 class UserReviewsViewModel: ViewModel() {
     val liveUserReviews = MutableLiveData<List<ReviewInfoItem>>()
+    val trustScore = MutableLiveData<Int>()
     private var page = 1
 
     fun loadUserReviews(authKey: String) {
@@ -71,6 +75,7 @@ class UserReviewsViewModel: ViewModel() {
                     page++
                 }
                 liveUserReviews.value = liveUserReviews.value?.plus(userInfo.reviews) ?: userInfo.reviews
+                trustScore.value = userInfo.trust_score
             } catch (e: Exception) {
                 println(e.stackTraceToString())
             }
@@ -81,13 +86,30 @@ class UserReviewsViewModel: ViewModel() {
 @Composable
 fun AccountScreen(viewModel: UserReviewsViewModel, authKey: String?, name: String?) {
     val userReviews by viewModel.liveUserReviews.observeAsState(initial = emptyList())
+    val trustScore by viewModel.trustScore.observeAsState(initial = 0)
 
     Column(
         modifier = Modifier.fillMaxWidth()
     ) {
         if (name != null) {
-            Text(name)
+            Text(
+                text = name,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(top = 15.dp),
+                fontSize = 24.sp
+            )
         }
+        Text(
+            text = "Trust Score: $trustScore",
+            modifier = Modifier.padding(top = 15.dp, start = 25.dp),
+            fontSize = 24.sp
+        )
+        Text(
+            text = "Reviews:",
+            modifier = Modifier.padding(top = 15.dp, start = 25.dp),
+            fontSize = 24.sp
+        )
 
         LazyColumn(
             modifier = Modifier
